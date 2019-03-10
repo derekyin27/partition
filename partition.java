f92117c727f3dca9158b4d4eaa7c078195a98b1d
Derek <dyin00@stuy.edu>
2019-03-10 13:23:45 -0400

    preconditions and new array

--- partition.java
+++ partition.java
@@ -1,3 +1,12 @@
 public class partition{
-  
+  public int partition(int[] data; int start; int end){
+    if (start<0 || start>= data.length || end < 0 || end >= data.length){
+      throw new IndexOutofBoundsException();
+    }
+    int[] newData = new int[end-start+1];
+    for (int i = start; i< end+1; i++){
+      newData[i] = data[i];
+    }
+    
+  }
 }
